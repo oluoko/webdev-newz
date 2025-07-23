7c14726fe39380b18859359ef4e4b801a86d734e
oluoko <brianotieno586@gmail.com>
2025-07-23 19:38:18 +0300

    Webdev Newz: Updated URL

--- pages/articles/[id]/index.js
+++ pages/articles/[id]/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import articleStyles from "@/styles/Article.module.css";
 import Link from "next/link";
-import { server } from "@/config";
 import Meta from "@/components/Meta";
 
 const article = ({ article }) => {
